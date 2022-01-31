94c41db5345037e20ae0beab186383556a9879cc
Nich Rosen <nicholasrosen234@gmail.com>
2022-01-30 22:34:39 -0800

    added comments

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args){
 
         count22NoOverlapResults();
-        count22OverlapResults();
-        factorsOf10Results();
+        count22OverlapResults();   //askdalsklhglwjhfljehfljwehljewhflkjweowefie0fjewqofjo[iejfoijefo]
+        factorsOf10Results();      //hfoueqhfouwehfiuwhfoqhfuqehfpdoufeofuehfoiuwhiofuhweifh
         balancedParensResults();
         //reverseArrayResults();
     }
